fb9e5c2971320152ff66c1b09703bae4761beef0
Leandro Martinez <lmartine@unicamp.br>
2024-12-18 16:10:14 -0300

    fix show test

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,5 +20,5 @@ end
     @test_throws AssertionError parse_show(a) ≈ "Object with Int(2), /usr/bin/bash and [1.0, 3.141592653589793, 7.5, 1.4142135623730951]"
     @test !isapprox(parse_show(a), "Object with Int(2), /usr/bin/bash and [1.0, 3.141592653589793, 7.5, 1.4142135623730951]"; assertion_error=false)
     # Test show method
-    @test contains(repr("text/plain", a), "Object with")
+    @test contains(repr("text/plain", parse_show(a)), "Object with Int( 1 )")
 end
