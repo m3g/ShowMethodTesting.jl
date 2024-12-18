ac63ec2fba865cb34ac535729e1aa17876a41c38
Leandro Martinez <lmartine@unicamp.br>
2024-12-18 15:03:17 -0300

    better docs

--- src/ShowMethodTesting.jl
+++ src/ShowMethodTesting.jl
@@ -1,15 +1,102 @@
 module ShowMethodTesting
 
-export parse_show
+export ParsedShow, parse_show
 
-struct TestShowString
+struct ParsedShow
     parsed_show::String
 end
-Base.show(io::IO, ::MIME"text/plain", x::TestShowString) = print(io, x.parsed_show)
+Base.show(io::IO, ::MIME"text/plain", x::ParsedShow) = print(io, x.parsed_show)
 
+"""
+    parse_show(x; vector_simplify=true, repl=Dict())
+    parse_show(x::String; vector_simplify=true, repl=Dict())
+
+Parse the output of `show` to a `ParsedShow` object, which can be compared with `isapprox` (`≈`).
+
+# Arguments
+
+- `x`: object to parse
+- `vector_simplify`: if `true`, only the first and last elements of arrays are kept
+- `repl`: dictionary with custom replacements to be made before parsing
+
+A `ParsedShow` object can be compared to another `ParsedShow` object or a string with `isapprox`.
+See the `isapprox` function for more details.
+
+# Example
+
+```jldoctest
+julia> using ShowMethodTesting
+
+julia> struct A
+           x::Int
+           path::String
+           vec::Vector{Float64}
+       end
+
+julia> Base.show(io::IO, ::MIME"text/plain", a::A) = print(io, "Object with Int(\$(a.x)), \$(a.path) and \$(a.vec)")
+
+julia> a = A(1, "/usr/bin/bash", [1.0, π, 7.5, √2])
+Object with Int(1), /usr/bin/bash and [1.0, 3.141592653589793, 7.5, 1.4142135623730951]
+
+julia> parse_show(a) ≈ "Object with Int(1), /usr/bin/bash and [1.0, 3.1415, 7.5, 1.4142]"
+true
+
+julia> isapprox(parse_show(a), "Object with Int(1), /usr/bin/bash and [1.0, 3.1415, 7.5, 1.4142]"; f64 = (x,y) -> x == y, assertion_error=false)
+false
+```
+
+Note that in the last line we have set the comparison function for floats to be exact, which is why the comparison fails. And the
+`assertion_error` is set to `false`, so the function returns `false` instead of throwing an error.
+
+"""
+function parse_show(x;
+    vector_simplify=true,
+    repl=Dict(),
+)
+    buff = IOBuffer()
+    show(buff, MIME"text/plain"(), x)
+    parse_show(String(take!(buff)); vector_simplify, repl)
+end
+
+function parse_show(x::String;
+    vector_simplify=true,
+    repl=Dict(),
+)
+    # Custom replacements
+    s = replace(x, repl...)
+    # add spaces between digits and other characters (except dots), to interpret them as numbers
+    s = replace(s, r"(?<=\d)(?=[^\d.])|(?<=[^\d.])(?=\d)" => s" ")
+    if vector_simplify # keep only first and last array elements
+        s = replace(s, r"\[ (\S+).* (\S+)\ ]" => s"[ \1 \2 ]")
+    end
+    return ParsedShow(s)
+end
+
+"""
+    isapprox(x::ParsedShow, y::ParsedShow
+        f64=(x1, x2) -> isapprox(x1, x2, rtol=1e-3),
+        i64=(x1, x2) -> x1 == x2,
+        path=(x1, x2) -> last(splitpath(x1)) == last(splitpath(x2)),
+        assertion_error=true,
+    )
+    isapprox(x::ParsedShow, y::String; kargs...) = isapprox(x, parse_show(y); kargs...)
+    isapprox(x::String, y::ParsedShow; kargs...) = isapprox(parse_show(x), y; kargs...)
+
+Compare two `ParsedShow` objects, with custom comparison functions for floats, integers and paths.
+
+# Arguments
+
+- `x`: first object to compare
+- `y`: second object to compare
+- `f64`: function to compare two floats
+- `i64`: function to compare two integers
+- `path`: function to compare two paths
+- `assertion_error`: if `true`, throws an `AssertionError` if the comparison fails
+
+"""
 function Base.isapprox(
-    x::TestShowString,
-    y::TestShowString;
+    x::ParsedShow,
+    y::ParsedShow;
     f64=(x1, x2) -> isapprox(x1, x2, rtol=1e-3),
     i64=(x1, x2) -> x1 == x2,
     path=(x1, x2) -> last(splitpath(x1)) == last(splitpath(x2)),
@@ -54,72 +141,7 @@ function Base.isapprox(
     end
     return all_match
 end
-Base.isapprox(x::TestShowString, y::String; kargs...) = isapprox(x, parse_show(y); kargs...)
-Base.isapprox(x::String, y::TestShowString; kargs...) = isapprox(parse_show(x), y; kargs...)
-
-"""
-    parse_show(x; vector_simplify=true, repl=Dict())
-    parse_show(x::String; vector_simplify=true, repl=Dict())
-
-Parse the output of `show` to a `TestShowString` object, which can be compared with `isapprox` (`≈`).
-
-# Arguments
-
-- `x`: object to parse
-- `vector_simplify`: if `true`, only the first and last elements of arrays are kept
-- `repl`: dictionary with custom replacements to be made before parsing
-
-# Comparison arguments
-
-The `isapprox` function comparing two `TestShowString` objects has the following keyword arguments:
-
-- `f64`: function to compare two floats
-- `i64`: function to compare two integers
-- `path`: function to compare two paths
-- `assertion_error`: if `true`, throws an `AssertionError` if the comparison fails
-
-# Example
-
-```jldoctest
-julia> using ShowMethodTesting
-
-julia> struct A
-           x::Int
-           path::String
-           vec::Vector{Float64}
-       end
-
-julia> Base.show(io::IO, ::MIME"text/plain", a::A) = print(io, "Object with Int(\$(a.x)), \$(a.path) and \$(a.vec)")
-
-julia> a = A(1, "/usr/bin/bash", [1.0, π, 7.5, √2])
-Object with Int(1), /usr/bin/bash and [1.0, 3.141592653589793, 7.5, 1.4142135623730951]
-
-julia> parse_show(a) ≈ "Object with Int(1), /usr/bin/bash and [1.0, 3.1415, 7.5, 1.4142]"
-true
-```
-
-"""
-function parse_show(x;
-    vector_simplify=true,
-    repl=Dict(),
-)
-    buff = IOBuffer()
-    show(buff, MIME"text/plain"(), x)
-    parse_show(String(take!(buff)); vector_simplify, repl)
-end
-
-function parse_show(x::String;
-    vector_simplify=true,
-    repl=Dict(),
-)
-    # Custom replacements
-    s = replace(x, repl...)
-    # add spaces between digits and other characters (except dots), to interpret them as numbers
-    s = replace(s, r"(?<=\d)(?=[^\d.])|(?<=[^\d.])(?=\d)" => s" ")
-    if vector_simplify # keep only first and last array elements
-        s = replace(s, r"\[ (\S+).* (\S+)\ ]" => s"[ \1 \2 ]")
-    end
-    return TestShowString(s)
-end
+Base.isapprox(x::ParsedShow, y::String; kargs...) = isapprox(x, parse_show(y); kargs...)
+Base.isapprox(x::String, y::ParsedShow; kargs...) = isapprox(parse_show(x), y; kargs...)
 
 end
